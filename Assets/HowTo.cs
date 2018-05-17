e1b846606e3bed13c58d78713483ed91bacd2be4
Arie Brian <ariebrian22@gmail.com>
2018-05-17 10:15:18 +0700

    init

---
+++ Assets/HowTo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class HowTo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetMouseButtonDown (0)){ 
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray,out hit,100.0f)) {
+				SceneManager.LoadScene("planet berputar v2", LoadSceneMode.Additive);
+			}
+		}*/
+	}
+
+	public void toHelp(){
+		SceneManager.LoadScene(1, LoadSceneMode.Single);
+	}
+}
---
+++ Assets/start.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*if (Input.GetMouseButtonDown (0)){ 
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray,out hit,100.0f)) {
+				SceneManager.LoadScene("planet berputar v2", LoadSceneMode.Additive);
+			}
+		}*/
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("menu", LoadSceneMode.Single);
+	}
+}
